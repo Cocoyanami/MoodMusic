e83b855d718c6930382834bfa4b0f007557ef546
Angel David Morales Cuenca <137020095+Cocoyanami@users.noreply.github.com>
2024-08-14 15:38:29 -0600

    Delete app directory

--- app/src/main/java/com/example/chatmusc/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.chatmusc
-
-object Constants {
-    val apiKey = "AIzaSyAjcNy_IqEQc_aJGP636cx0dhW0FDWqdvU"
-}
--- app/src/main/java/com/example/chatmusc/MessageModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chatmusc
-
-data class MessageModel(
-    val message : String,
-    val role : String
-)
