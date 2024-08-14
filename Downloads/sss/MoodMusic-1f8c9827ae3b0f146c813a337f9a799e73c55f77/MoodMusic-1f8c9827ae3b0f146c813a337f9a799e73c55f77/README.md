6924c2254c508ee8e4f7d6ac9da72754e72f900c
Angel David Morales Cuenca <137020095+Cocoyanami@users.noreply.github.com>
2024-08-14 15:39:13 -0600

    Delete Downloads/sss/MoodMusic-1f8c9827ae3b0f146c813a337f9a799e73c55f77/MoodMusic-1f8c9827ae3b0f146c813a337f9a799e73c55f77 directory

--- Downloads/sss/MoodMusic-1f8c9827ae3b0f146c813a337f9a799e73c55f77/MoodMusic-1f8c9827ae3b0f146c813a337f9a799e73c55f77/README.md
+++
@@ -1,2 +0,0 @@
-# MoodMusic
-# MoodMusic
--- Downloads/sss/MoodMusic-1f8c9827ae3b0f146c813a337f9a799e73c55f77/MoodMusic-1f8c9827ae3b0f146c813a337f9a799e73c55f77/app/src/main/java/com/example/chatmusc/Navigation/AppScreens.kt
+++
@@ -1,7 +0,0 @@
-package com.example.chatmusc.Navigation
-
-sealed class AppScreens (val route: String){
-    object LoginPage: AppScreens("login_page")
-    object SignUpPage: AppScreens("signup_page")
-    object ChatPage: AppScreens("chat_page")
-}
